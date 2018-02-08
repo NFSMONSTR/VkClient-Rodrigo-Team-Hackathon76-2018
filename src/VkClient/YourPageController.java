8e102dbd5114b11dea356795d8c4bb03c0028b5d
NFS_MONSTR <nfsmonstr@gmail.com>
2018-02-08 20:37:49 +0300

    added license

--- src/VkClient/YourPageController.java
+++ src/VkClient/YourPageController.java
@@ -1,3 +1,21 @@
+/*
+    VkClient - client for vk.com
+    Copyright (C) 2018  Belyaev Maxim(NFS_MONSTR), Aleksandr Novozhilov(MrLolthe1st), Grigoriy Zhukov
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package VkClient;
 
 import com.vk.api.sdk.objects.messages.Dialog;
